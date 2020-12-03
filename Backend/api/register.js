9e1d56529509ce8d49e3346e0a1966bf36691f28
big-bill <thapanon.sod@gmail.com>
2020-12-03 21:36:40 +0700

    55

--- Backend/api/register.js
+++ Backend/api/register.js
@@ -2,6 +2,12 @@ const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const { sql } = require("../database");
 
+router.post("/register", async(req, res => {
+    try {
 
+    } catch (err) {
+        return res.send(err);
+    }
+}))
 
 module.exports = router;

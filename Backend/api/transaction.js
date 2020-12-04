411feffa9e6576de3901212a8aeff40c9a1ea5f5
big-bill <thapanon.sod@gmail.com>
2020-12-04 21:09:11 +0700

    create traction js file

---
+++ Backend/api/transaction.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const { sql } = require("../database");
+
+router.post("/create", async(req, res) => {
+
+})
+
+router.post("/updateStatus/:id", async(req, res) => {
+
+})
+
+module.exports = router;

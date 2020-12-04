f8f693971e3d7e647bff5138ff931e0b9875e8fd
CasperX <casperx102@gmail.com>
2020-12-04 17:39:53 +0700

    fix typo and format code

--- Backend/database.js
+++ Backend/database.js
@@ -1,4 +1,7 @@
-require("dotenv").config({ silent: process.env.NODE_ENV === "production" });
+require("dotenv").config({
+  silent: process.env.NODE_ENV === "production" 
+});
+
 const mariadb = require("mariadb");
 
 const pool = mariadb.createPool({
